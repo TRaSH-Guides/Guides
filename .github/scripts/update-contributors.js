858221fe1b4f53290641d7d5122e4fb75fb17a4a
FonduemangVI <michael.thomas@vualtlabs1226.com>
2024-03-31 23:09:55 +1100

    more testing

--- .github/scripts/update-contributors.js
+++ .github/scripts/update-contributors.js
@@ -12,14 +12,31 @@
         throw new Error('Invalid response from GitHub API');
       }
   
-      const contributors = response.data.map(user => {
+      let contributors = '<table>\n<tr>\n';
+  
+      response.data.forEach((user, index) => {
         if (!user.login || !user.html_url || !user.avatar_url) {
           throw new Error('Invalid user data from GitHub API');
         }
-        return `| ![${user.login}](${user.avatar_url}&s=100) | [@${user.login}](${user.html_url}) |`;
-      }).join('\n');
   
-      fs.writeFileSync('CONTRIBUTORS.md', `# Contributors\n\n| Avatar | Username |\n| --- | --- |\n${contributors}\n`);
+        contributors += `
+          <td align="center">
+            <a href="${user.html_url}">
+              <img src="${user.avatar_url}&s=100" width="100;" alt="${user.login}"/>
+              <br />
+              <sub><b>${user.login}</b></sub>
+            </a>
+          </td>`;
+  
+        // Add a new row every 6th user
+        if ((index + 1) % 6 === 0) {
+          contributors += '\n</tr>\n<tr>\n';
+        }
+      });
+  
+      contributors += '</tr>\n</table>';
+  
+      fs.writeFileSync('CONTRIBUTORS.md', `## Contributors\n\n<!-- readme: contributors -start -->\n${contributors}\n<!-- readme: contributors -end -->\n`);
     } catch (error) {
       console.error('Error updating contributors:', error.message);
   
