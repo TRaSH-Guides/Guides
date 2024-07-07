7ba9c202764660b780fdfde6c0364257433f9278
nuxen <theonenuxen@proton.me>
2024-07-07 12:47:17 +0200

    fix(ci): multiple commits causing issues

--- .github/scripts/update-changelog.sh
+++ .github/scripts/update-changelog.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-log="$1"
-base_url="https://github.com/TRaSH-Guides/Guides/pull/"
+log_file="$1"
 output_file="docs/updates.txt"
 current_time=$(date +"%F %H:%M")
 formatted_output="# $current_time"
 
 while IFS= read -r message; do
-    pr_number=$(echo "$message" | grep -oE '#[0-9]+' | tr -d '#')
-    formatted_message=$(echo "$message" | sed -E 's/ \(#.*//')
+    pr_number=$(echo "$message" | grep -oE '\(#[0-9]+\)$' | tr -d '(#)')
+    formatted_message=$(echo "$message" | sed -E 's/ \(#[0-9]+\)$//')
     formatted_output+="
-- [$formatted_message](${base_url}${pr_number})"
-done <<< "$log"
+- [$formatted_message](https://github.com/TRaSH-Guides/Guides/pull/${pr_number})"
+done < "$log_file"
 
 formatted_output+="\n"
 
@@ -19,6 +18,3 @@ formatted_output+="\n"
     echo -e "$formatted_output"
     cat "$output_file"
 } > "${output_file}.tmp" && mv "${output_file}.tmp" "$output_file"
-
-# Inform the user
-echo "Output prepended to $output_file"
